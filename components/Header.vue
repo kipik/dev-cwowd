7a3353140d9bf3ca8614a6eb301ba9a57ecd027e
kipik <kipik.kipik@gmail.com>
2021-09-03 14:25:35 +0200

    Composant Header
    Viré le side-menu

---
+++ components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+    <client-only>
+        <v-app-bar dark app dense>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+            <v-toolbar-title>
+                <NuxtLink to="/" class="a">Cwowd</NuxtLink>
+            </v-toolbar-title>
+            <v-subheader class="subheader">Ludique et participatif</v-subheader>
+      
+            <v-spacer />
+      
+            <div class="navbar-right">
+                <ul class="navbar-nav">
+                    <li v-for="tag in tags" :key="tag.id" class="links">
+                        <router-link 
+                        :to="{ name: 'tags-id', params: {id: tag.id} }" 
+                        tag="a" 
+                        class="a">
+                        {{ tag.name }}
+                        </router-link>
+                    </li>
+                </ul>
+      </div>
+        </v-app-bar>
+
+  </client-only>
+</template>
+
+<script>
+import tagsQuery from '~/apollo/queries/tag/tags'
+
+export default {
+    apollo: {
+        tags: {
+        prefetch: true,
+        query: tagsQuery 
+        }
+    },
+    data () {
+        return {
+            tags: []
+        }
+    }  
+}
+</script>
+<style scoped>
+.a {
+  text-decoration: none;
+  color: honeydew;
+}
+.navbar-nav {
+  display: flex;
+}
+
+.links {
+  display: flex;
+  justify-content: space-around;
+  padding: 0 10px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,106 +1,27 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      expand-on-hover
-      fixed 
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-     
-    <v-app-bar dark app dense>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title>
-        <NuxtLink to="/" class="a">Cwowd</NuxtLink>
-      </v-toolbar-title>
-      <v-subheader class="subheader">Ludique et participatif</v-subheader>
-      <v-spacer />
-      <div class="navbar-right">
-        <ul class="navbar-nav">
-          <li v-for="tag in tags" :key="tag.id" class="links">
-            <router-link 
-              :to="{ name: 'tags-id', params: {id: tag.id} }" 
-              tag="a" 
-              class="a">
-              {{ tag.name }}
-            </router-link>
-          </li>
-        </ul>
-      </div>
-    </v-app-bar>
-
+    <Header />
+    
     <v-main>
       <v-container fluid>
         <Nuxt />
       </v-container>
     </v-main>
 
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+    <v-footer>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
+    
   </v-app>
 </template>
 
 <script>
-import tagsQuery from '~/apollo/queries/tag/tags'
+import Header from '~/components/Header.vue'
 
 export default {
+  components: {
+    Header
+  },
   data () {
     return {
       clipped: false,
@@ -122,29 +43,12 @@ export default {
       right: true,
       rightDrawer: false,
       title: 'Vuetify.js',
-      tags: [],
-    }
-  },
-  apollo: {
-    tags: {
-      prefetch: true,
-      query: tagsQuery 
     }
   }
 }
 </script>
 
 <style scoped>
-.navbar-nav {
-  display: flex;
-}
-
-.links {
-  display: flex;
-  justify-content: space-around;
-  padding: 0 10px;
-}
-
 .a {
   text-decoration: none;
   color: honeydew;
